1b84916784836a967d8229a15d4d399223d5fd10
Nguyen Van Hiep <hiep12092001@gmail.com>
2019-09-17 15:00:40 +0700

    commit main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,6 +7,10 @@ document.addEventListener('DOMContentLoaded', ()=>{
         let result = await fetch(`https://graph.facebook.com/${id}/comments?fields=id&limit=5000&access_token=${token}`);
         return result.json();
     }
+    async function react_comments(id_comment, token){
+        let result = await fetch(`https://graph.facebook.com/v4.0/${id_comment}/reactions?type=WOW&method=post&access_token=${token}`);
+        return result.json();
+    }
     var stm = document.getElementById('submit');
     stm.addEventListener('click',()=>{
         const ACCESS_TOKEN = document.getElementById('access_token').value;
@@ -18,6 +22,7 @@ document.addEventListener('DOMContentLoaded', ()=>{
                 .then(res0=>{
                     if(res0.id && res0.name){
                         toastr_comment();
+                        
                     }else{
                         toastr.error('Invalid ACCESS_TOKEN', {timeOut: 3000});
                     }
@@ -30,9 +35,23 @@ document.addEventListener('DOMContentLoaded', ()=>{
                     .then(res1=>{
                         var length_comment = res1.data.length;
                         toastr.info(`Found: ${length_comment}`, {timeOut: 3000});
+                        
+                        for(let i = 0; i < length_comment; i++){
+                            let id_cmt = res1.data[i].id;
+                            if(id_cmt.split('_')[1]){
+                                fetch(`https://graph.facebook.com/v4.0/${id_cmt}/reactions?type=${REACT}&method=post&access_token=${ACCESS_TOKEN}`)
+                           
+                            }else{
+                                fetch(`https://graph.facebook.com/v4.0/${ID}_${id_cmt}/reactions?type=${REACT}&method=post&access_token=${ACCESS_TOKEN}`)
+                           
+                            }
+                        }
+                       setTimeout(()=>{
+                        toastr.info(`Done!`, {timeOut: 3000});
+                       }, 6000);
                     })
                     .catch(err1=>{
-                        console.log(err);
+                        console.log(err1);
                     })
                 }
             }else if(!ACCESS_TOKEN){
